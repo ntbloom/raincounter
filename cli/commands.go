f9ab851286ddf74d6b41bf7d876d4814f9740a73
ntbloom <ntbloom@gmail.com>
2021-08-19 23:31:21 -0400

    moved cli to its own directory

---
+++ cli/commands.go
@@ -0,0 +1,17 @@
+package cli
+
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{} //nolint:gochecknoglobals
+
+// Configure sets global CLI configs
+func Configure() {
+	RootCmd.CompletionOptions.DisableDefaultCmd = true
+}
+
+// AddSubcommand adds a subcommand to the CLI
+func AddSubcommand(command string, short string, callable func()) {
+	RootCmd.AddCommand(&cobra.Command{Use: command, Short: short, Run: func(_ *cobra.Command, _ []string) {
+		callable()
+	}})
+}
--- main.go
+++ main.go
@@ -4,22 +4,15 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/ntbloom/raincounter/cli"
+
 	"github.com/ntbloom/raincounter/pkg/gateway"
-	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{} //nolint:gochecknoglobals
-
-func addSubcommand(command string, short string, callable func()) {
-	rootCmd.AddCommand(&cobra.Command{Use: command, Short: short, Run: func(_ *cobra.Command, _ []string) {
-		callable()
-	}})
-}
-
 func main() {
-	addSubcommand("gateway", "shuffle data from sensor to mqtt on the gateway", gateway.Start)
-
-	if err := rootCmd.Execute(); err != nil {
+	cli.Configure()
+	cli.AddSubcommand("gateway", "shuffle data from sensor to mqtt on the gateway", gateway.Start)
+	if err := cli.RootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 	}
 }
