6234821d12f0df6383f70fc0375e79f185da4d04
ntbloom <ntbloom@gmail.com>
2021-10-11 22:44:49 -0400

    moved viper to after cobra

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 )
 
 func main() {
-	config.Configure()
 	cli.Configure()
 	cli.AddSubcommand("rainbase", "shuffle data from sensor to MQTT on the rainbase", rainbase.Start)
 	cli.AddSubcommand("receiver", "receive data over MQTT on the cloud", raincloud.Receive)
 	cli.AddSubcommand("server", "serve the rest API on the cloud", raincloud.Serve)
+
+	config.Configure()
 	if err := cli.RootCmd.Execute(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 	}
