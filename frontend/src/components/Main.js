89ea2762095f658db416314d3b8fc0d72a38fd34
ntbloom <ntbloom@gmail.com>
2021-10-12 21:19:30 -0400

    brought in shell of front end

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,15 @@
+/** @format */
+
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
