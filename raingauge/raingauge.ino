d6fdd78bbd887bf530fab319b6991720b8c166d0
ntbloom <ntbloom@gmail.com>
2022-03-02 23:10:48 -0500

    rebuilding loop from scratch with board

--- raingauge/raingauge.ino
+++ raingauge/raingauge.ino
@@ -1,10 +1,30 @@
+/* arduino_rain_gauge.ino
+ *
+ * Increment rain gauge counter on click, display on screen
+ *
+ */
+
+#include <Arduino.h>
+
+/* specify the board to be used */
+#include "src/boards/nano33ble.cpp"
+
+void ledSetup(void)
+{
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+}
 
 void setup()
 {
-    delay(1000);
+    ledSetup();
 }
 
 void loop()
 {
+    // digitalWrite(LED_RED, (millis() / 1000) % 2);   // blink the LED every 5 seconds
+    // digitalWrite(LED_BLUE, (millis() / 1000) % 2);  // blink the LED every 5 seconds
+    digitalWrite(LED_GREEN, (millis() / 1000) % 2); // blink the LED every 5 seconds
 }
 
---
+++ raingauge/src/boards/nano33ble.cpp
@@ -0,0 +1,9 @@
+/* for a MKR1000 board, configure the board with the following pins */
+
+#define PAUSE_PIN 5
+#define TEMP_PIN A2
+#define TEMP_VOLTAGE 3.274
+#define RAIN_PIN 9
+#define LED_GREEN 2
+#define LED_RED 3
+#define LED_BLUE 4
